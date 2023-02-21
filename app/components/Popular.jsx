fa89ca7b72ca5a9d5480cba72e9a1ff1e5b2536b
Carlos Regis <cr@carlosregis.com>
2023-02-21 19:19:12 -0100

    3-navbar-state

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,16 +1,38 @@
 import * as React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage: selectedLanguage,
+    });
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-      <select name="" id="">
-        {languages.map((language) => (
-          <option key={language} value={language}>
-            {language}
-          </option>
-        ))}
-      </select>
+      <main>
+        <select
+          onChange={(event) => this.updateLanguage(event.target.value)}
+          selected={this.state.selectedLanguage}
+        >
+          {languages.map((language) => (
+            <option key={language} value={language}>
+              {language}
+            </option>
+          ))}
+        </select>
+        {JSON.stringify(this.state.selectedLanguage, null, 2)}
+      </main>
     );
   }
 }
