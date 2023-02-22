70eaafa5bac2bf0bddb921b90aeff582a3f41127
Carlos Regis <cr@carlosregis.com>
2023-02-21 23:25:45 -0100

    5-languages-nav-proptypes

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 function LanguagesNav({ selectedLanguage, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -16,6 +17,11 @@ function LanguagesNav({ selectedLanguage, onUpdateLanguage }) {
     </select>
   );
 }
+
+LanguagesNav.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
 
 export default class Popular extends React.Component {
   constructor(props) {
