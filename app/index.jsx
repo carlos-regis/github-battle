fb598baa32594d5523e964c9207978eaf3401589
Carlos Regis <cr@carlosregis.com>
2023-05-03 17:05:23 +0000

    add routes

--- app/index.jsx
+++ app/index.jsx
@@ -3,17 +3,23 @@ import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="light">
-        <div className="container">
-          <Popular />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="light">
+                    <div className="container">
+                        <Routes>
+                            <Route path="/" element={<Popular />} />
+                            <Route path="/battle" element={<Battle />} />
+                        </Routes>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
